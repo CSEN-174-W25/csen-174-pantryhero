339fd8853b9427cc9311ed73818933e0bea314f9
Lucas Amlicke <32651190+rukasuamarike@users.noreply.github.com>
2025-01-20 00:25:11 -0800

    Added prisma routes and tableview

--- pantryhero/app/page.tsx
+++ pantryhero/app/page.tsx
@@ -1,14 +1,31 @@
-export default function Home() {
-  return (
-    <div
-      style={{
-        maxWidth: 1280,
-        margin: '0 auto',
-        padding: '2rem',
-        textAlign: 'center',
-      }}
-    >
-     Pantry Hero
-    </div>
-  );
+import Tableview from "@/components/tableview";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function Home() {
+  try {
+    const food = await prisma.ingredients.findMany();
+    const formattedFood = food.map((item) => ({
+      ...item,
+      barcode: item.barcode ? item.barcode.toString() : null, 
+    }));
+
+    return (
+      <div
+        style={{
+          maxWidth: 1280,
+          margin: "0 auto",
+          padding: "2rem",
+          textAlign: "center",
+        }}
+      >
+        <h1>Pantry Hero</h1>
+        <Tableview food={formattedFood} />
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return <div>Error loading data</div>;
+  }
 }
