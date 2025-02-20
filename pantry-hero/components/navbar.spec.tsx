475f4f80da79256518f3778727f81790ca05d6d8
yqgg <yiqiangoh12@gmail.com>
2025-02-19 19:04:39 -0800

    Added speed dial and removed scan from navbar
    The speed dial includes a link to the scanner and a link to the add recipe page. The cookbook icon now links to a new page where it will display the user inputted recipes instead of the add recipe page.

--- pantry-hero/components/navbar.spec.tsx
+++ pantry-hero/components/navbar.spec.tsx
@@ -10,24 +10,29 @@ describe("Navbar", () => {
     expect(screen.getByRole("navigation")).toBeInTheDocument();
 
     // Check if the navigation links are rendered
-    expect(screen.getByRole("link", { name: "Scan" })).toBeInTheDocument();
-    expect(screen.getByRole("link", { name: "Home" })).toBeInTheDocument();
     expect(screen.getByRole("link", { name: "Pantry" })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "Home" })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "Cookbook" })).toBeInTheDocument();
   });
 
   it("updates the active navigation link", () => {
     render(<Navbar />);
 
     // Check the initial active link
-    const ScanLink = screen.getByRole("link", { name: "Scan" });
-    expect(ScanLink).toBeInTheDocument();
+    const CookbookLink = screen.getByRole("link", { name: "Cookbook" });
+    expect(CookbookLink).toBeInTheDocument();
 
     // Click on the "Home" link
     const HomeButton = screen.getByRole("link", { name: "Home" });
     fireEvent.click(HomeButton);
 
+    const PantryLink = screen.getByRole("link", { name: "Pantry"});
+    expect(PantryLink).toBeInTheDocument();
+
+
     // Check if the "Home" link is now active
+    expect(PantryLink).not.toHaveClass("Mui-selected");
     expect(HomeButton).toHaveClass("Mui-selected");
-    expect(ScanLink).not.toHaveClass("Mui-selected");
+    expect(CookbookLink).not.toHaveClass("Mui-selected");
   });
 });
--- pantry-hero/components/navbar.tsx
+++ pantry-hero/components/navbar.tsx
@@ -5,7 +5,16 @@ import HomeIcon from '@mui/icons-material/Home';
 import CropFreeIcon from '@mui/icons-material/CropFree';
 import KitchenIcon from '@mui/icons-material/Kitchen';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
 import Link from "next/link";
+
+const actions = [
+  { icon: <CropFreeIcon />, name: 'Add To Your Pantry', href: "/scan" },
+  { icon: <EditNoteIcon />, name: 'Add To Your Cookbook', href: "/recipe" },
+];
 
 export default function Navbar() {
   const [value, setValue] = React.useState(1);
@@ -20,28 +29,38 @@ export default function Navbar() {
           setValue(newValue);
         }}
       >
-        <BottomNavigationAction
-          label="Scan"
-          icon={<CropFreeIcon/>}
-          component={Link}
-          href="/scan"
-        />
-        <BottomNavigationAction 
-        label="Home" 
-        icon={<HomeIcon/>} 
-        component={Link}
-        href="/"/>
         <BottomNavigationAction 
         label="Pantry" 
         icon={<KitchenIcon/>} 
         component={Link}
         href="/pantry"/>
+
+        <BottomNavigationAction 
+        label="Home" 
+        icon={<HomeIcon/>} 
+        component={Link}
+        href="/"/>
+        
         <BottomNavigationAction
         label="Cookbook"
         icon={<MenuBookIcon/>}
         component={Link}
         href="/cookbook"/>
       </BottomNavigation>
+      
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        sx={{ position: 'absolute', bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon />}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            onClick={() => window.location.href = action.href}
+            tooltipTitle={action.name}/>
+        ))}
+      </SpeedDial>
     </Paper>
   );
 }
